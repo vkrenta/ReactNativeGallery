3b5bcffa0f87f21ca2e0ef9b70ea421f2fc50932
vkrenta <0vkrenta@gmail.com>
2020-02-20 16:36:46 +0200

    deleted static rows

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,13 +3,6 @@ import {FlatList, View} from 'react-native';
 import Item from './Item';
 import getAllDocuments from '../api';
 
-const rows = [
-  {id: 0, text: 'fb.com'},
-  {id: 1, text: 'pornhub.com'},
-  {id: 2, text: 'github.com'},
-  {id: 3, text: 'google.com'},
-];
-
 const extractKey = ({id}) => id;
 
 const renderItem = ({item}) => {
